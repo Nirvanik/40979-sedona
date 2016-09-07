ccfeb842e74d20bb8dc709eae4ca87a3183668e3
Aleander Nirvanik <nirvanik77@gmail.com>
2016-09-07 14:45:19 +0300

    подготовил к допуску макет для сдачи

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-var link = document.querySelector(".search");
-  var popup = document.querySelector(".search-content");
-
-          link.addEventListener("click", function(event) {
-            event.preventDefault();
-            popup.classList.toggle("search-content-show");
-          });
+var link=document.querySelector(".search"),popup=document.querySelector(".search-content");link.addEventListener("click",function(a){a.preventDefault(),popup.classList.toggle("search-content-show")});
